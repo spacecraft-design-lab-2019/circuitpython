03a2624f6ee90e31866aa89b579b6e21ced927f2
Andrew Gatherer <gatherer@stanford.edu>
2020-02-10 18:16:12 -0800

    zac fixed it

--- shared-bindings/ulab/controller.c
+++ shared-bindings/ulab/controller.c
@@ -25,13 +25,6 @@
 	// this function takes in an array of the bdot and magnetic moment
 	// and modifies the moment in place
 	
-	uint8_t* tmp = calloc(length, sizeof(uint8_t));
-
-	for(size_t i = 0; i < 6; i++) {
-		*(tmp+i) = 5;
-    }
-    free(c);
-    *c = *tmp;
 
 }*/
 
@@ -40,34 +33,17 @@ mp_obj_t ulab_controller_bdot(mp_obj_t self_in) {
     ulab_ndarray_obj_t *self = MP_OBJ_TO_PTR(self_in); 
     // this passes in the array
     // the size of a single item in the array
-    uint8_t _sizeof = mp_binary_get_size('@', self->array->typecode, NULL);
+    //uint8_t _sizeof = mp_binary_get_size('@', self->array->typecode, NULL);
     
     // NOTE: 
     // we assume that the array passed in is of the form [u[1:3],Bdot[1:3]]
-    uint8_t *c = (uint8_t *)self->array->items;
+    mp_float_t *c = (mp_float_t *)self->array->items;
 
-    if((self->m != 1) && (self->n != 6)) {
-    	mp_raise_ValueError(translate("array is wrong size"));
+    for(size_t i=0; i < self->array->len; i++) {
+    	c[i] = -c[i];
     }
-	else {
-	    // self->bytes is the size of the bytearray, irrespective of the typecode
-	    uint8_t length = 6;
+            
+    return MP_OBJ_FROM_PTR(self);
 
-	    //agnotstic to function
-        uint8_t *tmp = m_new(uint8_t, self->bytes);
-	    for(size_t n=0; n < length; n++) {
-	    	tmp[n] = 1;
-	    }
-	    
-        for(size_t n=0; n < self->n; n++) {
-            memcpy(c+_sizeof*(n + 1), &tmp[n], _sizeof);
-        }
-
-	    // copy back to circuitpython
-	    memcpy(self->array->items, c, self->bytes);
-	    m_del(uint8_t, c, self->bytes);
-	}
-
-    return mp_const_none;
 }
 
